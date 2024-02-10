b8699b31e13ebb02fea9c1f62656d861b44349d3
Slawomir Kasprzak <slawek.kasprzak@icloud.com>
2024-02-10 17:10:08 +0000

    created offer module

---
+++ src/main/java/com/junioroffers/domain/offer/OfferSavingException.java
@@ -0,0 +1,20 @@
+package com.junioroffers.domain.offer;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class OfferSavingException extends RuntimeException {
+    private final List<String> offerUrls;
+
+    public OfferSavingException(String offerUrl) {
+        super(String.format("Offer with offerUrl [%s] already exist", offerUrl));
+        this.offerUrls = List.of(offerUrl);
+    }
+
+    public OfferSavingException(String message, List<Offer> offers) {
+        super(String.format("error" + message + offers.toString()));
+        this.offerUrls = offers.stream().map(Offer::offerUrl).toList();
+    }
+}
